9257e6ba18f86533407c447f91663e3453fcdd7e
Dominic Masters <dominic@domsplace.com>
2019-01-23 18:26:20 +1100

    Very early proof of concept, showing how the scenes could be setup. Need to start impl. sockets.

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "roots": [
+    "<rootDir>/private",
+    "<rootDir>/public"
+  ],
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json"
+  ],
+}
